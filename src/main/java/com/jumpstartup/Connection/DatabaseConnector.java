8ecec9df927e221acc6f288f7b22ce74f4d7a0af
Nikhil Sharma <nikhilsharma@v1040-wn-rt-a-0-27.campus-dynamic.uwaterloo.ca>
2023-02-24 18:33:39 -0500

    added the db connection

---
+++ src/main/java/com/jumpstartup/Connection/DatabaseConnector.java
@@ -0,0 +1,25 @@
+package com.jumpstartup.Connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnector {
+    private static final String DB_URL = "jdbc:h2:file:./jsudb.h2.db";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection != null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                System.out.println("Error while trying to close database connection: " + e.getMessage());
+            }
+        }
+    }
+}
--- src/main/java/com/jumpstartup/Database/LoginDatabase.java
+++ src/main/java/com/jumpstartup/Database/LoginDatabase.java
@@ -1,16 +1,18 @@
 package com.jumpstartup.Database;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import com.jumpstartup.Connection.DatabaseConnector;
+
 
 public class LoginDatabase {
     public boolean authenticate(String username, String password) {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection("jdbc:h2:file:./jsudb.h2.db", "sa", "");
+            connection = DatabaseConnector.getConnection();
             String sql = "SELECT * FROM myUser WHERE username = ? AND hashpass = ?";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, username);
@@ -26,23 +28,14 @@ public class LoginDatabase {
             System.out.println("Error while trying to authenticate user: " + e.getMessage());
             return false;
         } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.out.println("Error while trying to close database connection: " + e.getMessage());
-                }
-            }
+            DatabaseConnector.closeConnection(connection);
         }
     }
 
     public boolean newUser(String UUID,String username, String email, String hashpass, String type) {
         Connection connection = null;
-
-        System.out.println(UUID+","+username+ "," + email+","+hashpass+"," + type );
-
         try {
-            connection = DriverManager.getConnection("jdbc:h2:file:./jsudb.h2.db", "sa", "");
+            connection = DatabaseConnector.getConnection();
             String sql = "INSERT INTO myUser(UUID, username, email, hashpass, type) VALUES(?,?, ?, ?, ?)";
             PreparedStatement statement = connection.prepareStatement(sql);
             statement.setString(1, UUID);
@@ -61,16 +54,7 @@ public class LoginDatabase {
             System.out.println("Error while trying to add new user: " + e.getMessage());
             return false;
         } finally {
-            if (connection != null) {
-                try {
-                    connection.close();
-                } catch (SQLException e) {
-                    System.out.println("Error while trying to close database connection: " + e.getMessage());
-                }
-            }
+            DatabaseConnector.closeConnection(connection);
         }
     }
-
-
-
 }
