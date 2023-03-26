bfa136558bd4953a5df87cba92827e6c258f1725
Nikhil Sharma <nikhilsharma@v1040-wn-rt-a-127-162.campus-dynamic.uwaterloo.ca>
2023-03-25 20:37:38 -0400

    added cases to DBTEST

--- src/test/java/com/jumpstartup/jumpstartupjava/DBTest.java
+++ src/test/java/com/jumpstartup/jumpstartupjava/DBTest.java
@@ -1,7 +1,7 @@
 package com.jumpstartup.jumpstartupjava;
 
 import com.jumpstartup.Connection.DatabaseConnector;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -11,26 +11,63 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DBTest {
 
+    private Connection conn;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        conn = DatabaseConnector.getConnection();
+    }
+
+    @AfterEach
+    void tearDown() throws SQLException {
+        if (conn != null && !conn.isClosed()) {
+            DatabaseConnector.closeConnection(conn);
+        }
+    }
+
     @Test
-    void testGetConnection() throws SQLException {
-        Connection conn = DatabaseConnector.getConnection();
+    void testGetConnection() {
         assertNotNull(conn);
-        assertTrue(conn.isValid(10));
-        DatabaseConnector.closeConnection(conn);
+        assertTimeoutPreemptively(
+                java.time.Duration.ofSeconds(10),
+                () -> assertTrue(conn.isValid(10))
+        );
     }
 
     @Test
     void testCloseConnection() throws SQLException {
-        Connection conn = DatabaseConnector.getConnection();
-        assertNotNull(conn);
         DatabaseConnector.closeConnection(conn);
         assertTrue(conn.isClosed());
+    }
+
+    @Test
+    void testCloseNullConnection() throws SQLException {
+        Connection nullConnection = null;
+        assertDoesNotThrow(() -> DatabaseConnector.closeConnection(nullConnection));
+    }
+
+    @Test
+    void testGetDbUrl() {
+        assertEquals("jdbc:h2:mem:jsudb", DatabaseConnector.getDbUrl());
     }
 
     @Test
     void testInvalidCredentials() {
         assertThrows(SQLException.class, () -> {
             Connection conn = DriverManager.getConnection(DatabaseConnector.getDbUrl(), "invalid_user", "invalid_password");
+        });
+    }
+
+
+    @Test
+    void testDoubleCloseConnection() {
+        assertDoesNotThrow(() -> {
+            Connection conn = DatabaseConnector.getConnection();
+            assertNotNull(conn);
+            DatabaseConnector.closeConnection(conn);
+            assertTrue(conn.isClosed());
+            // Attempt to close the connection again, which should throw an SQLException
+            DatabaseConnector.closeConnection(conn);
         });
     }
 
